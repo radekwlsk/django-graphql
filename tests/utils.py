6cb76154c3c1a9bc41d4b73e9b00160aca8e4b16
radoslaw.kowalski <radoslaw.kowalski@10clouds.com>
2019-12-27 13:13:43 +0100

    on tenc_graphene settings override reload schema and view also not only urls

--- tests/utils.py
+++ tests/utils.py
@@ -15,8 +15,8 @@ def override_graphene_settings(**settings):
 
 @contextlib.contextmanager
 def override_tenc_graphene_settings(**settings):
-    return _override_settings(tenc_graphene_settings, callback_before=reload_urls, callback_after=reload_urls,
-                              **settings)
+    return _override_settings(tenc_graphene_settings, callback_before=reload_tenc_graphene,
+                              callback_after=reload_tenc_graphene, **settings)
 
 
 def _override_settings(settings_module, callback_before=None, callback_after=None, **settings):
@@ -63,10 +63,36 @@ def _override_settings(settings_module, callback_before=None, callback_after=Non
         callback_after()
 
 
-def reload_urls(urlconf=None):
-    from tenc_graphene import urls, settings as tenc_graphene_settings
-    clear_url_caches()
+def reload_tenc_graphene(**kwargs):
+    reload_settings(**kwargs)
+    reload_schema(**kwargs)
+    reload_urls(**kwargs)
+
+
+def reload_settings():
+    from tenc_graphene import settings as tenc_graphene_settings
+    from graphene_django import settings as graphene_django_settings
+    reload(graphene_django_settings)
     reload(tenc_graphene_settings)
+
+
+def reload_schema():
+    from tenc_graphene import schema, views
+    from graphene_django import views as graphene_django_views
+
+    reload(schema)
+    if 'tests.schema' in sys.modules:
+        reload(sys.modules['tests.schema'])
+    else:
+        import_module('tests.schema')
+    reload(graphene_django_views)
+    reload(views)
+
+
+def reload_urls(urlconf=None):
+    from tenc_graphene import urls
+
+    clear_url_caches()
     reload(urls)
     if urlconf is None:
         urlconf = settings.ROOT_URLCONF
@@ -74,3 +100,4 @@ def reload_urls(urlconf=None):
         reload(sys.modules[urlconf])
     else:
         import_module(urlconf)
+
