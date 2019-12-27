1790b25f2979403cf735fe6639578677a5cccbab
radoslaw.kowalski <radoslaw.kowalski@10clouds.com>
2019-12-27 13:14:14 +0100

    inline import of class for comparison due to reloading on settings override

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,7 +1,6 @@
 import pytest
 from django.urls import resolve
 
-from tenc_graphene.views import TencGrapheneView
 from .utils import override_tenc_graphene_settings
 
 
@@ -9,11 +8,13 @@ class TestUserURLs:
 
     def test_foo_url_path(self):
         with override_tenc_graphene_settings(URL_PATH='foo/'):
+            from tenc_graphene.views import TencGrapheneView
             found = resolve('/graphql/foo/')
             assert found.func.view_class == TencGrapheneView
 
     def test_empty_url_path(self):
         with override_tenc_graphene_settings(URL_PATH=''):
+            from tenc_graphene.views import TencGrapheneView
             found = resolve('/graphql/')
             assert found.func.view_class == TencGrapheneView
 
